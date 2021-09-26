103412b997fd50ded1b671457435b6a62484edc3
cama0047 <cama0047@algonquinlive.com>
2021-09-25 23:59:39 -0400

    adding searchbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
-import { Chat, DonutLarge, MoreVert } from "@material-ui/icons";
-import { Avatar } from "@mui/material";
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from "@material-ui/icons";
+import { Avatar, IconButton } from "@mui/material";
 
 import React from "react";
 import "./Sidebar.css";
@@ -11,13 +11,25 @@ function Sidebar() {
         <Avatar />
 
         <div className="sidebar__headerRight">
-          <DonutLarge />
-          <Chat />
-          <MoreVert />
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
         </div>
       </div>
 
-      <div className="sidebar__search"></div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start a new chat " type="text" />
+        </div>
+      </div>
+
       <div className="sidebar__chats"></div>
     </div>
   );
