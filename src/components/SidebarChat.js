d7df532f11c5666fb67b3ced8c451003133eb9c3
cama0047 <cama0047@algonquinlive.com>
2021-09-26 00:44:08 -0400

    adding Avatars to sidebarchats

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -1,22 +1,29 @@
 import { Avatar } from '@mui/material';
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import "./Sidebar.css";
 
 const SidebarChat = ()=> {
 
+    const [seed, setSeed] = useState('')
+
+
+    useEffect(() => {
+        
+       setSeed(Math.floor(Math.random()*1000));
+    }, [])
 
 
     return (
-        
-        <div className="sidebarChat">
-            <Avatar />
-            <div className="sidebar__info">
-                <h2>Room name</h2>
-                <p>Last message..</p>
-            </div>
+      <div className="sidebarChat">
+        <Avatar
+          src={`https://avatars.dicebear.com/api/personas/${seed}.svg`}
+        />
+        <div className="sidebar__info">
+          <h2>Juan</h2>
+          <p>Last message..</p>
         </div>
-        
-    )
+      </div>
+    );
 }
 
 export default SidebarChat
