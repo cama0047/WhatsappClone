c4a610db2d0d0528219a61eefc85ec1ba4561c0a
cama0047 <cama0047@algonquinlive.com>
2021-10-04 02:53:28 -0400

    adding auth with google :D

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Login.css";
 import {Button} from "@material-ui/core"
+import { auth, provider } from "../Firebase";
 
 function Login() {
 
 const signIn = () =>{
 
-}
+    auth
+      .signInWithPopup(provider)
+      .then((result) => console.log(result))
+      .catch((error) => console.log(error));
+};
+
 
   return (
     <div className="login">
